9820d6b86e295d35db25995477f481e66384b724
isakal <sakalivan4@gmail.com>
2021-01-27 12:38:53 +0100

    Remove duplicate route binding

--- app/home/routes.py
+++ app/home/routes.py
@@ -5,7 +5,6 @@ from random import randint
 home = Blueprint('home', __name__)
 
 
-@home.route('/home')
 @home.route('/', methods=['GET', 'POST'])
 def homepage():
     return render_template('home.html', title='Home')
