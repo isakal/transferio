5c11d1d7e057c2766c761940e98e5bce9c1faa70
isakal <sakalivan4@gmail.com>
2021-01-31 19:14:45 +0100

    Make the prototype frontend and backend
    Made the prototype frontend just to test input types and backend
    mechanics regarding inputs.

--- app/home/routes.py
+++ app/home/routes.py
@@ -1,5 +1,5 @@
+from datetime import datetime
 from flask import Blueprint, render_template, request
-from random import randint
 
 
 home = Blueprint('home', __name__)
@@ -7,4 +7,13 @@ home = Blueprint('home', __name__)
 
 @home.route('/', methods=['GET', 'POST'])
 def homepage():
-    return render_template('home.html', title='Home')
+    today = datetime.today()
+    if request.method == "POST":
+        departure = request.form.get("departure")
+        destination = request.form.get("destination")
+        num_of_pass = request.form.get("number_of_passengers")
+        departure_date = request.form.get("departure_date")
+
+        return render_template('home.html', message=f'{departure} {destination} {num_of_pass} {departure_date}', title='Home', today=today)
+
+    return render_template('home.html', title='Home', today=today)
